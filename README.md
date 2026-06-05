2166c47f51c6ac24059e3e5ad1db0835df4697c2
DmRafaule <ChedrDen@gmail.com>
2026-06-05 10:47:01 +0300

    Added an ability to convert text into QR code

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ QRify bot made for generating QR codes and reading an existing ones from photo a
 More detailed description you may find on the website of the developer [QR to text and vise versa](https://timthewebmaster.com/en/tools/)
 
 ## Basic features
-* Convert a input text into QR code
+* Convert an input text into QR code
 * Convert images with QR code into text
 * Inline mode, this means you can use this bot in any chat to be able to covert your text into QR code
--- main.py
+++ main.py
@@ -1,14 +1,40 @@
+import io
 import sys
 import logging
 import asyncio
+import qrcode
+from qrcode.constants import ERROR_CORRECT_L
 
-from aiogram import Dispatcher, Bot
+from aiogram import Dispatcher, Bot, F
+from aiogram.types import Message, BufferedInputFile
 
 bot_dispatcher = Dispatcher()
 with open(".env", "r") as file:
     buffer = file.read()
     line_pos = buffer.find("BOT_TOKEN")
     TOKEN = buffer[buffer.find("=", line_pos) + 1:buffer.find("\n", line_pos)]
+
+
+@bot_dispatcher.message(F.text)
+async def text2qr(message: Message):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=ERROR_CORRECT_L,
+    )
+    qr.add_data(message.text)
+    qr.make(fit=True)
+    generated_img = qr.make_image(fill_color="black", back_color="white")
+
+    bytes = io.BytesIO()
+    generated_img.save(bytes, 'PNG')
+    bytes.seek(0)
+
+    img = BufferedInputFile(
+        bytes.getvalue(), 
+        filename="qrcode-buffer.png"
+    )
+    await message.answer(f"QR: '{message.text}'")
+    await message.answer_photo(img)
 
 async def main() -> None:
     bot = Bot(TOKEN)
